f0be2fbc501730cd565b18280b90e3538cb356fa
Timur <tb.makuha@gmail.com>
2023-05-21 12:28:21 +0500

    Updated remove footer bug comments

--- pages/form_page.py
+++ pages/form_page.py
@@ -16,7 +16,8 @@ class FormPage(BasePage):
     def fill_form_fields(self):
         person = next(generated_person())
         file_name, path = generated_file()
-        self.remove_footer()
+        # For successfully pass the test, we need remove the footer
+        # self.remove_footer()
         self.element_is_visible(self.locators.FIRST_NAME).send_keys(person.first_name)
         self.element_is_visible(self.locators.LAST_NAME).send_keys(person.last_name)
         self.element_is_visible(self.locators.EMAIL).send_keys(person.email)
